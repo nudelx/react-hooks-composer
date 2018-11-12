6dda24e8db500ead193074832e896f6fe5ab0a1b
nudelx <nudelx@gmail.com>
2018-11-12 14:28:59 +0200

    npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   mode: 'production',
   entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   output: {
-    filename: '[name].js',
-    path: __dirname + '/lib'
+    // filename: '[name].js',
+    // path: __dirname + '/lib'
+    filename: 'index.js',
+    library: 'mylib',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
   }
 }
